8caa9a1070a10f69209319f4b7cca412c2991cc2
Katie O'Flynn <koflynn@spartaglobal.co>
2017-02-23 15:41:11 +0000

    lazy loading

--- spec/addressbook_spec.rb
+++ spec/addressbook_spec.rb
@@ -1,6 +1,16 @@
 describe AddressBook do
+
+  let(:person1) { Person.new("Serah", "Farron", "1 Jan 1990") }
+  let(:person2) { Person.new("Claire", "Farron", "22 March 1987") }
+
   it "should initialise properly" do
     book = AddressBook.new
+    expect(book.address_book).to eq []
+  end
+
+  it "should allow adding of peaople" do
+    book.add person1
+    book.add person2
     expect(book.address_book).to eq []
   end
 end
--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -1,37 +1,34 @@
 describe Person do
+
+  let(:person) { Person.new("Serah", "Farron") }
+
   it "should initialise properly" do
-    person = Person.new("Serah", "Farron")
     expect(person.first_name).to eq "Serah"
     expect(person.surname).to eq "Farron"
     expect(person.dob).to eq nil
   end
 
   it "should give us a person's full name" do
-    person = Person.new("Serah", "Farron")
     expect(person.fullname).to eq "Serah Farron"
   end
 
   it "should allow us to add an email" do
-    person = Person.new("Serah", "Farron")
     person.add_email "nora@bodhum.net"
     expect(person.emails).to eq ["nora@bodhum.net"]
   end
 
   it "should allow us to add a phone number" do
-    person = Person.new("Serah", "Farron")
     person.add_phone "5656424"
     expect(person.phone_numbers).to eq ["5656424"]
   end
 
   it "should allow us to delete an email" do
-    person = Person.new("Serah", "Farron")
     person.add_email "nora@bodhum.net"
     person.remove_email 0
     expect(person.emails).to eq []
   end
 
   it "should allow us to remove a phone number" do
-    person = Person.new("Serah", "Farron")
     person.add_phone "5656424"
     person.remove_phone 0
     expect(person.phone_numbers).to eq []
